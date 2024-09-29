6135b956a28cdef1d9078cd20ecf19ae18909efb
Arisha-chadha <arishachadha13@gmail.com>
2024-09-29 15:28:37 +0530

    Create Pattern 17.py

---
+++ Lab 4/Pattern 17.py
@@ -0,0 +1,13 @@
+#17 Downward Triangle Pattern of Stars
+#        * * * * * * 
+#        * * * * * 
+#         * * * * 
+#          * * * 
+#           * * 
+#            * 
+n = 6
+for i in range(n):
+    print(" " * i, end='')
+    for j in range(n - i):
+        print("*", end=' ')
+    print()
