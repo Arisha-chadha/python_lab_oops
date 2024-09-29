bb292c62a1eb307cac783c8ea30748ab4dd62cf2
Arisha-chadha <arishachadha13@gmail.com>
2024-09-29 12:40:45 +0530

    Update Pattern 8.py

--- Lab 4/Pattern 8.py
+++ Lab 4/Pattern 8.py
@@ -1,1 +1,14 @@
+#Pyramid of Natural Numbers Less Than 10
+#1 
+#2 3 4 
+#5 6 7 8 9
+n = 3  
+count = 1  
+
+for i in range(1, n + 1):
+    for j in range(1, 2 * i):  
+        if count < 10: 
+            print(count, end=" ")
+            count += 1
+    print()
 
