fc0237fc742e82a0b0af90d19031fa16d38ad8aa
Arisha-chadha <arishachadha13@gmail.com>
2024-09-29 12:30:36 +0530

    Update Pattern 5.py

--- Lab 4/Pattern 5.py
+++ Lab 4/Pattern 5.py
@@ -1,6 +1,12 @@
 #Inverted Pyramid of the Same Digit
 #5 5 5 5 5 
-# 5 5 5 5 
-#  5 5 5 
-#   5 5 
-#    5
+#  5 5 5 5 
+#    5 5 5 
+#      5 5 
+#        5
+n = 5  
+for i in range(n, 0, -1):
+    print(" " * (n - i) * 2, end="")
+    for j in range(1, i+1):
+        print(5, end=" ")
+    print()
