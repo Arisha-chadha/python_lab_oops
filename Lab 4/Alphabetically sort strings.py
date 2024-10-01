cad89080a506ef3a066d23abc6ebc47dd9b3057b
Arisha-chadha <arishachadha13@gmail.com>
2024-10-01 10:25:58 +0530

    Create Alphabetically sort strings.py

---
+++ Lab 4/Alphabetically sort strings.py
@@ -0,0 +1,5 @@
+# Write a program to sort strings alphabetically in python
+strings = ["banana", "apple", "cherry", "date"]
+strings_sorted = sorted(strings)
+
+print("Sorted strings:", strings_sorted)
